e5ffdb73a81afaa3ada1773848a9c66d5f58bd4e
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-10 20:49:05 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const qnas = document.querySelectorAll(".qna");
+
+qnas.forEach((eachQna) => {
+	eachQna.addEventListener("click", () => {
+		console.dir(eachQna);
+		let qnaValue = eachQna.dataset.value;
+		let qnaAnswer = eachQna.lastElementChild;
+		let qnaIcon = eachQna.firstElementChild.lastElementChild;
+
+		// !changing plus & minus icons
+
+		switch (qnaValue) {
+			case "inactive":
+				qnaAnswer.classList.add("show-answer");
+				eachQna.dataset.value = "active";
+				qnaIcon.src = "/assets/images/icon-minus.svg";
+				break;
+			case "active":
+				qnaAnswer.classList.remove("show-answer");
+				eachQna.dataset.value = "inactive";
+				qnaIcon.src = "/assets/images/icon-plus.svg";
+				break;
+			default:
+				break;
+		}
+	});
+});
